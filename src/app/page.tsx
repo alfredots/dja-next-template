0cb7ed508e7d5d37febfc4b8478a030def6ab080
Alfredo Tito <tito.alfred@hotmail.com>
2023-12-03 17:30:51 -0300

    chore: testing git hooks

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  const test = '';
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h2>DJA NEXT TEMPLATE</h2>
+    </main>
+  );
+}
